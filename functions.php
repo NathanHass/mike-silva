36ac5bb44a2d80006792808720a2786cc66f389c
Nathan Hass <nathan@upstatement.com>
2016-10-29 14:47:00 -0400

    first commit

---
+++ functions.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+$timber = new \Timber\Timber();
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+			echo '<div class="error"><p>Timber is not installed.  Run composer `composer install` from the root of the theme to install Timber.</p></div>';
+		} );
+	return;
+}
+
+Timber::$dirname = array('templates', 'views');
+
+class MikeSilvaSite extends Timber\Site {
+
+	function __construct() {
+		add_theme_support( 'post-formats' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		parent::__construct();
+	}
+
+	function register_post_types() {
+		//this is where you can register custom post types
+	}
+
+	function register_taxonomies() {
+		//this is where you can register custom taxonomies
+	}
+
+	function add_to_context( $context ) {
+		$context['foo'] = 'bar';
+		$context['stuff'] = 'I am a value set in your functions.php file';
+		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
+		$context['menu'] = new Timber\Menu();
+		$context['site'] = $this;
+		return $context;
+	}
+
+	function myfoo( $text ) {
+		$text .= ' bar!';
+		return $text;
+	}
+
+	function add_to_twig( $twig ) {
+		/* this is where you can add your own fuctions to twig */
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
+		return $twig;
+	}
+
+}
+
+new MikeSilvaSite();
